0c1d7792619639af63320fc24b310e65a9842130
Ayşe Nur Akdeniz <a.nurakdeniz@hotmail.com>
2022-02-03 20:23:31 +0300

    added basic functions such as gray image, gassian blur, canny and  dilation

---
+++ basic_functions/basic_functions.cpp
@@ -0,0 +1,34 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(){
+
+    string path="schumi.jpg";
+    Mat img = imread(path);
+
+    Mat img_gray, img_blur, img_canny, img_dilate, img_erode;
+
+    cvtColor(img, img_gray, COLOR_BGR2GRAY);
+
+    GaussianBlur(img, img_blur, Size(3, 3), 3, 0);
+
+    Canny(img_blur, img_canny, 25, 75);
+
+    Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
+
+    dilate(img_canny, img_dilate, kernel);
+
+    imshow("Original Image", img);
+    imshow("Gray Image", img_gray);
+    imshow("Gaussian Blur Image", img_blur);
+    imshow("Canny Image", img_canny);
+    imshow("Dilation Image", img_dilate);
+
+    waitKey(0);
+}
